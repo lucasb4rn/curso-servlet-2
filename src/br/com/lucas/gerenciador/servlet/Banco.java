6a6e2e51fa7f7c47a0b74a07515de3bafa16c877
Lucasb4rngit config --global user.email Lucas.b4rn@gmail.comgit config --global user.name Lucasb4rn <Lucas.b4rn@gmail.com>
2019-02-03 22:15:11 -0200

    Adicionando o delete com redirect

--- src/br/com/lucas/gerenciador/servlet/Banco.java
+++ src/br/com/lucas/gerenciador/servlet/Banco.java
@@ -1,29 +1,44 @@
 package br.com.lucas.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<Empresa>();
+	private static Integer chaveSequencial = 1;
 
-	
 	static {
 		Empresa empresa1 = new Empresa();
+		empresa1.setId(chaveSequencial++);
 		empresa1.setNome("Empresa1");
 		lista.add(empresa1);
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Empresa2");
 		lista.add(empresa2);
 	}
-	
+
 	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
 		lista.add(empresa);
+
 	}
-	
-	
-	public List<Empresa> getEmpresas(){
+
+	public void removeEmpresa(Integer id) {
+
+		Iterator<Empresa> it = lista.iterator();
+
+		while (it.hasNext()) {
+			Empresa empresa = it.next();
+			if (empresa.getId() == id)
+				it.remove();
+		}
+	}
+
+	public List<Empresa> getEmpresas() {
 		return Banco.lista;
 	}
-	
+
 }
