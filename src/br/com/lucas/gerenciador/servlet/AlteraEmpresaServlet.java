920cd396a80e8295e0fe9c355c374e9ae0fd5f2e
Lucasb4rngit config --global user.email Lucas.b4rn@gmail.comgit config --global user.name Lucasb4rn <Lucas.b4rn@gmail.com>
2019-02-03 22:46:48 -0200

    Crud finalizado implementado alterarEmpresa

---
+++ src/br/com/lucas/gerenciador/servlet/AlteraEmpresaServlet.java
@@ -0,0 +1,53 @@
+package br.com.lucas.gerenciador.servlet;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/alteraEmpresa")
+public class AlteraEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		System.out.println("Alterando Empresa");
+		
+		String nomeEmpresa = request.getParameter("nome");
+		String paramDataEmpresa = request.getParameter("data");
+		String paramId = request.getParameter("id");
+		Integer id = Integer.valueOf(paramId);
+		
+		
+		SimpleDateFormat sdt = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Date dataAbertura = null;
+		
+		try {
+			dataAbertura = sdt.parse(paramDataEmpresa);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+		
+		Banco banco = new Banco();
+		
+		Empresa empresa = banco.buscaEmpresaPeloId(id);
+		
+		empresa.setNome(nomeEmpresa);
+		empresa.setDataAbertura(dataAbertura);
+		
+		response.sendRedirect("listaEmpresas");
+		
+		System.out.println(id);
+		
+		
+	
+	}
+
+}
