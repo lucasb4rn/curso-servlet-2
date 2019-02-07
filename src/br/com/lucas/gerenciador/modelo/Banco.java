87f5994307d8be300b7d470c9a78b28b1aac04c7
Lucasb4rngit config --global user.email Lucas.b4rn@gmail.comgit config --global user.name Lucasb4rn <Lucas.b4rn@gmail.com>
2019-02-06 22:34:52 -0200

    Adicionando formulario de login

--- src/br/com/lucas/gerenciador/modelo/Banco.java
+++ src/br/com/lucas/gerenciador/modelo/Banco.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<Empresa>();
+	private static List<Usuario> listaUsuario = new ArrayList<Usuario>();
 	private static Integer chaveSequencial = 1;
 
 	static {
@@ -18,6 +19,18 @@ public class Banco {
 		empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Empresa2");
 		lista.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("lucas");
+		u1.setSenha("12345");
+		Usuario u2 = new Usuario();	
+		u2.setLogin("ana");
+		u2.setSenha("12345");
+		
+		listaUsuario.add(u1);
+		listaUsuario.add(u2);
+				
+		
 	}
 
 	public void adiciona(Empresa empresa) {
