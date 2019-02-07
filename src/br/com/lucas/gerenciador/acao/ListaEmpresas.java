f69a887a0204b7938da3a6750f571ae354e948d3
Lucasb4rngit config --global user.email Lucas.b4rn@gmail.comgit config --global user.name Lucasb4rn <Lucas.b4rn@gmail.com>
2019-02-06 23:57:26 -0200

    Implementando a autonização de acesso um lugar centralizado para não copiar código, no caso controlor

--- src/br/com/lucas/gerenciador/acao/ListaEmpresas.java
+++ src/br/com/lucas/gerenciador/acao/ListaEmpresas.java
@@ -16,11 +16,6 @@ public class ListaEmpresas implements Acao {
 	
 	public String executa(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		
-		HttpSession sessao = request.getSession();
-		if(sessao.getAttribute("usuarioLogado") == null) {
-			return "redirect:entrada?acao=LoginForm";
-		}
-		
 		System.out.println("Listando Empresas");
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
