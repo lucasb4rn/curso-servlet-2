f57b8b5c2590b54f64905ff5acc1fb3d2de0c152
Lucasb4rngit config --global user.email Lucas.b4rn@gmail.comgit config --global user.name Lucasb4rn <Lucas.b4rn@gmail.com>
2019-02-06 23:42:47 -0200

    Implementanção bloqueio para formLogin se não estiver autenticado

--- src/br/com/lucas/gerenciador/acao/ListaEmpresas.java
+++ src/br/com/lucas/gerenciador/acao/ListaEmpresas.java
@@ -7,6 +7,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.lucas.gerenciador.modelo.Banco;
 import br.com.lucas.gerenciador.modelo.Empresa;
@@ -14,6 +15,11 @@ import br.com.lucas.gerenciador.modelo.Empresa;
 public class ListaEmpresas implements Acao {
 	
 	public String executa(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		
+		HttpSession sessao = request.getSession();
+		if(sessao.getAttribute("usuarioLogado") == null) {
+			return "redirect:entrada?acao=LoginForm";
+		}
 		
 		System.out.println("Listando Empresas");
 		Banco banco = new Banco();
